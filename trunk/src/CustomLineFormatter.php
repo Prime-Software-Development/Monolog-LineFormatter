edf2f239a13ea98377b89941d832a107932af1f0
chris <chris@c426c41b-92c4-4fd5-9393-d7958c5c828f>
2018-10-17 08:48:51 +0000

    Initial commit

---
+++ trunk/src/CustomLineFormatter.php
@@ -0,0 +1,20 @@
+<?php
+namespace TrunkSoftware\Monolog\CustomLineFormatter;
+
+use Monolog\Formatter\LineFormatter;
+use Monolog\Logger;
+
+class CustomLineFormatter extends LineFormatter {
+
+	public function format( array $record ) {
+		$output = parent::format( $record );
+
+		if ( $record[ 'level' ] >= Logger::ERROR ) {
+			$output = "<span style='color:red;font-weight: bold;'>" . $output .  "</span>";
+		}
+		elseif ( $record[ 'level' ] >= Logger::WARNING) {
+			$output = "<span style='color:orangered;font-weight: bold;'>" . $output .  "</span>";
+		}
+		return $output;
+	}
+}
